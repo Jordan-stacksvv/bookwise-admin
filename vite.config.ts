b96007525208e9593afa3b2c84515c7c83ce7e63
Jordan Owusu-Ansah <jordanowusu03@gmail.com>
2026-05-22 12:17:10 +0000

    Fix redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config"; export default defineConfig({ tanstackStart: { server: { entry: "server" } } });
+import { defineConfig } from "@lovable.dev/vite-tanstack-config"; export default defineConfig({ tanstackStart: { server: { entry: "server" } }, vite: { publicDir: "public" } });
